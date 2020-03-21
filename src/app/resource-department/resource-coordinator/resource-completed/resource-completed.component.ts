e175567161a9c8390df52dd0b4edc39d04d9d6da
swamyakunoori <swan.my88@gmail.com>
2020-03-21 14:25:52 +0530

    department->coordinate->completed projet

--- src/app/resource-department/resource-coordinator/resource-completed/resource-completed.component.ts
+++ src/app/resource-department/resource-coordinator/resource-completed/resource-completed.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GridOptions } from 'ag-grid-community';
+
+import { CompletedProjectsService } from 'src/app/_services/resource-department/resource-coordinator/completed-projects.service';
 
 @Component({
   selector: 'app-resource-completed',
@@ -7,9 +10,122 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResourceCompletedComponent implements OnInit {
 
-  constructor() { }
+  breadcrumb: any;
+  completedProjectsList = [];
+  agGridOptions: GridOptions;
+
+  private GridApi;
+  private GridColumnApi;
+  public columnDefs;
+
+  constructor(private _completedProjectsService: CompletedProjectsService) {
+    this.completedProjectsGridInit();
+  }
 
   ngOnInit() {
+    this.breadcrumb = {
+      'links': [
+        {
+          'name': 'Home',
+          'isLink': true,
+          'link': '/dashboard/sales'
+        },
+      ],
+      'mainlabel': 'Completed Projects'
+    };
+    
+    this.getcompletedProjectsList();
   }
+
+  completedProjectsGridInit() {
+    this.agGridOptions = <GridOptions>{
+      context: {
+        componentParent: this
+      },
+    };
+
+    this.columnDefs = [
+      {
+        headerName: "",
+        width: 35,
+        pinned: "left",
+        suppressSizeToFit: true,
+        headerCheckboxSelectionFilteredOnly: true,
+        editable: false,
+        checkboxSelection: function (params) {
+          return params.columnApi.getRowGroupColumns().length === 0;
+        }
+      },
+      {
+        headerName: 'PROJECT ID',
+        field: 'project_id',
+        sortable: true,
+        filter: true
+      },
+      {
+        headerName: 'REGIONAL MANAGER',
+        field: 'manager_id',
+        sortable: true,
+        filter: true
+      },
+      {
+        headerName: 'PROJECT TYPE',
+        field: 'project_type',
+        sortable: true,
+        filter: true
+      },
+      {
+        headerName: 'PROJECT STATUS',
+        field: 'status',
+        sortable: true,
+        filter: true
+      },
+      {
+        headerName: 'COMPLETION DATE',
+        field: 'completionDate',
+        sortable: true,
+        filter: true
+      },
+      {
+        headerName: 'DURATION(HRS)',
+        field: 'duration',
+        sortable: true,
+        filter: true
+      },
+      {
+        headerName: 'QUESTIONS SOLVED',
+        field: 'questionsSolved',
+        sortable: true,
+        filter: true
+      }
+    ];
+    this.agGridOptions.rowSelection = "single";
+    this.agGridOptions.paginationPageSize = 10;
+    this.agGridOptions.rowData = [];
+  };
+
+  getcompletedProjectsList(){
+     this._completedProjectsService.getcompletedProjectsList()
+      .subscribe((data: any) =>{
+        this.completedProjectsList = data.result;
+        this.autoSizeAll();
+        this.GridApi.sizeColumnsToFit();
+      })
+  };
+
+  autoSizeAll() {
+    var allColumnIds = [];
+    this.GridColumnApi.getAllColumns().forEach(function (column) {
+        allColumnIds.push(column.colId);
+    });
+    this.GridColumnApi.autoSizeColumns(allColumnIds);
+}
+
+onGridReady(params) {
+    this.GridApi = params.api;
+    this.GridColumnApi = params.columnApi;
+    this.autoSizeAll();
+    this.GridApi.sizeColumnsToFit();
+}
 
 }
